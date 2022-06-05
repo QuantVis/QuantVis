db7f5e80b918ff71dc66449ec3517be7fbdc4bf2
seinJOO <sein19950910@gmail.com>
2022-06-06 03:12:21 +0900

    user login update
    로그인 관련 완성~~ 임시 테이블 create 코드는 아래 참고
    create table user( num bigint auto_increment primary key, id varchar(100), email varchar(100) UNIQUE, password varchar(200), nick varchar(50), method varchar(50), my_stock varchar(10000));

--- DB/userDB.py
+++ DB/userDB.py
@@ -14,16 +14,18 @@ class User(UserMixin):
                 'use_unicode':True
             }   
 
-    def get_user_info(self, user_email):
-        print(f'select : {user_email}')
+    def get_user_info(self, user_id = '',user_email = ''):
         try:
                 conn = pymysql.connect(**self.config)
                 cursor = conn.cursor()
-                sql = f"select * from user where email = '{user_email}'"
+                if user_email != '' :
+                    sql = f"SELECT * from user where email = '{user_email}'"
+                elif user_id != '' :
+                    sql = f"SELECT * from user where id = '{user_id}'"
                 cursor.execute(sql)
                 res = cursor.fetchone()
                 if not res : return None                
-                user = {'user_id' : res[1], 'user_email' : res[2], 'nick' : res[3], 'method' : res[4], 'my_stock' : res[5]}
+                user = {'id' : res[1], 'email' : res[2], 'password' : res[3], 'nick' : res[4], 'method' : res[5], 'my_stock' : res[6]}
                 return user
 
         except Exception as e:
@@ -32,11 +34,11 @@ class User(UserMixin):
             cursor.close()
             conn.close()
     
-    def create_user_info(self, user_id, email, method) :
+    def create_user_info(self, user_id,  method, email='', password='', nick='') :
         try:
                 conn = pymysql.connect(**self.config)
                 cursor = conn.cursor()
-                sql = f"INSERT INTO user(id, email, method) VALUES ('{user_id}', '{email}', '{method}')"
+                sql = f"INSERT INTO user(id, email, password, nick, method) VALUES ('{user_id}', '{email}', '{password}', '{nick}', '{method}')"
                 result = cursor.execute(sql)                
                 if result == 1 : conn.commit()
                 return result
@@ -47,13 +49,34 @@ class User(UserMixin):
         finally:            
             cursor.close()
             conn.close()
+    
+    def nick_check(self, nick) :
+        try:
+            conn = pymysql.connect(**self.config)
+            cursor = conn.cursor()
+            sql = f"SELECT id FROM user WHERE nick = '{nick}'"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            return result
+        
+        except Exception as e:
+            print("DB연동 에러 : ", e)
+            conn.rollback()
+        finally:            
+            cursor.close()
+            conn.close()
             
-    def update_nick(self, user_id, nick) :
+    ## 로컬가입용 정보변경
+    def set_change1(self, email, nick='', password='') :
         try:
             conn = pymysql.connect(**self.config)
             cursor = conn.cursor()
-            sql = f"UPDATE user SET nick = '{nick}' WHERE id = '{user_id}'"
-            result = cursor.execute(sql)                
+            if nick != '' : sql = f"UPDATE user SET nick = '{nick}' WHERE email = '{email}'"
+            result = cursor.execute(sql)
+            if result == 1 : conn.commit()
+            
+            if password != '' : sql = f"UPDATE user SET password = '{password}' WHERE email = '{email}'"
+            result = cursor.execute(sql)
             if result == 1 : conn.commit()
             return result
         
@@ -63,13 +86,19 @@ class User(UserMixin):
         finally:            
             cursor.close()
             conn.close()
-        
-    def update_email(self, user_id, email) :
+    
+    ## 소셜로그인용 정보변경
+    def set_change2(self, user_id, nick='', email='') :
         try:
             conn = pymysql.connect(**self.config)
             cursor = conn.cursor()
-            sql = f"UPDATE user SET email = '{email}' WHERE id = '{user_id}'"
-            result = cursor.execute(sql)                
+            if nick != '' and email != '' : 
+                sql = f"UPDATE user SET email = '{email}', nick = '{nick}' WHERE id = '{user_id}'"
+            elif nick != '' : 
+                sql = f"UPDATE user SET nick = '{nick}' WHERE id = '{user_id}'"
+            elif email != '' : 
+                sql = f"UPDATE user SET email = '{email}' WHERE id = '{user_id}'"
+            result = cursor.execute(sql)
             if result == 1 : conn.commit()
             return result
         
@@ -79,4 +108,20 @@ class User(UserMixin):
         finally:            
             cursor.close()
             conn.close()
+            
+    def find_pw(self, email, nick) :
+        try:
+            conn = pymysql.connect(**self.config)
+            cursor = conn.cursor()
+            sql = f"SELECT * FROM user WHERE email = '{email}' and nick = '{nick}'"
+            result = cursor.execute(sql)
+            return result
+        
+        except Exception as e:
+            print("DB연동 에러 : ", e)
+            conn.rollback()
+        finally:            
+            cursor.close()
+            conn.close()
+        
     
