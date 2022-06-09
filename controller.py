48aefe13d8914ae475db2c375d07c420e55171f1
Young5620 <95738245+Young5620@users.noreply.github.com>
2022-06-09 15:15:29 +0900

    Revert "portfolio_01_push"
    This reverts commit d8400ad8adcd319513c3480aa8fd998aa45ae351.

--- controller.py
+++ controller.py
@@ -4,13 +4,11 @@ from datetime import timedelta
 import os, requests
 from module.user_login import user_login     #** 해당 파일에서 적용한 변수명으로 가져옴
 from module.strategy_1 import strategy_1
-from module.portfolio import port
 
 # App config
 app = Flask(__name__)
 app.register_blueprint(user_login)  #** 블루프린트 적용 
 app.register_blueprint(strategy_1)
-app.register_blueprint(port)
 
 # Session config
 app.secret_key = os.urandom(24)
